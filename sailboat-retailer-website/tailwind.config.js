48e859ffc08eb96225d359de86f4d7a1ec06261a
atila-tokyo <atilamanager@gmail.com>
2022-07-04 10:54:36 +0900

    Adding project starter with styling and images

--- sailboat-retailer-website/tailwind.config.js
+++ sailboat-retailer-website/tailwind.config.js
@@ -1,21 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [',/src/{*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Josefin Sans', 'sans-serif'],
-        alata: ['Alata']
-      }
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
